8a7e7c0f5c0d682ea0ed603202acf111ba8852f3
yxtay <5795122+yxtay@users.noreply.github.com>
2025-08-21 18:08:47 +0800

    Revert "fix(data): movielens-url"
    This reverts commit 84c4bdb984462c75110805f0d9068f37587f36f1.

--- seq_rec/data/prepare.py
+++ seq_rec/data/prepare.py
@@ -7,9 +7,7 @@ from concurrent.futures import ThreadPoolExecutor
 import polars as pl
 from loguru import logger
 
-from seq_rec.params import DATA_DIR
-
-MOVIELENS_1M_URL = "http://files.grouplens.org/datasets/movielens/ml-1m.zip"
+from seq_rec.params import DATA_DIR, MOVIELENS_1M_URL
 
 ###
 # download data
